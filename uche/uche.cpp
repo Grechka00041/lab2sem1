def50d8decdd57c4009970ecbf1cb2231cf65acc
Grechka00041 <irinamuzycenko34@gmail.com>
2024-10-24 20:03:17 +0300

    Add files via upload
    second version

--- uche/uche.cpp
+++ uche/uche.cpp
@@ -7,14 +7,15 @@ using namespace chrono;
 void mainMenu() {
     system("cls");
     cout << "Выберите действие: " << endl;
-    cout << "1. Вывести элементы массива" << endl;
-    cout << "2. Сортировка массива, поиск максимального и минимального элементов, поиск индексов и количесива элементов, равных среднему значению максимального и минимального" << endl;
-    cout << "3. Найти в отсортированном массиве количество элементов, которые меньше введенного вами числа" << endl;
-    cout << "4. Найти в отсортированном массиве количество элементов, которые больше введенного вами числа" << endl;
-    cout << "5. Бинарный поиск введенного числа в массиве" << endl;
-    cout << "6. Поменять элементы массива местами" << endl;
-    cout << "7. Уменьшить нечетные элементы на выбранное значение, умножить их на рандомное число, вывести делимость элементов " << endl;
-    cout << "8. Выход" << endl;
+    cout << "1. Создать новый массив" << endl;
+    cout << "2. Вывести элементы массива" << endl;
+    cout << "3. Сортировка массива, поиск максимального и минимального элементов, поиск индексов и количества элементов, равных среднему значению максимального и минимального" << endl;
+    cout << "4. Найти в отсортированном массиве количество элементов, которые меньше введенного вами числа" << endl;
+    cout << "5. Найти в отсортированном массиве количество элементов, которые больше введенного вами числа" << endl;
+    cout << "6. Бинарный поиск введенного числа в массиве" << endl;
+    cout << "7. Поменять элементы массива местами" << endl;
+    cout << "8. Уменьшить нечетные элементы на выбранное значение, умножить их на рандомное число, вывести делимость элементов " << endl;
+    cout << "9. Выход" << endl;
     cout << "Введите число, соответствующее нужному вам действию: ";
 }
 void choosingSortMenu() {
@@ -28,7 +29,7 @@ void choosingSortMenu() {
     cout << "6. Выход" << endl;
     cout << "Введите число, соответствующее нужному вам действию: ";
 }
-int checkInput(int ceiling, int floor) {
+int checkInput(int ceiling = 2147483647, int floor = -2147483647) {
     int num;
     cin >> num;
     while (cin.fail() || cin.peek() != '\n' || num > ceiling || num < floor) {
@@ -89,6 +90,8 @@ void MinMaxAverageForSorted(int arr[], int N) {
             amount = amount + 1;
             cout << i << " ";
         }
+        if (arr[i] > average)
+            break;
     }
     auto end1 = steady_clock::now();
     auto result1 = duration_cast<nanoseconds>(end1 - start1);
@@ -241,16 +244,25 @@ int main() {
     int choice;
     do {
         mainMenu();
-        choice = checkInput(8, 1);
-        int end = n - 1, begin = 0;
+        choice = checkInput(9, 1);
+        int endSort = n - 1, beginSort = 0;
         switch (choice) {
         case 1:
-            cout << "Вот все элементы массива: ";
-            for (int i = 0; i < n; ++i)
-                cout << A[i] << "; ";
+            cout << "Вот новый массив: ";
+            for (int i = 0; i < n; ++i) {
+                A[i] = rand() % 198 - 99;
+                cout << A[i] << ' ';
+            }
             cout << endl;
             break;
-        case 2:    
+
+        case 2:
+            cout << "Вот все элементы массива: ";
+            for (int i = 0; i < n; ++i)
+                cout << A[i] << " ";
+            cout << endl;
+            break;
+        case 3:    
             int choice2;
             do {
                 choosingSortMenu();
@@ -260,28 +272,28 @@ int main() {
                     cout << "Без сортировки: " << endl;
                     MinMaxAverage(A, n);
                     bubbleSort(A, n);
-                    cout << "С сортировкой: " << endl;
+                    cout << "\nС сортировкой: " << endl;
                     MinMaxAverageForSorted(A, n);
                     break;
                 case 2:  
                     cout << "Без сортировки: " << endl;
                     MinMaxAverage(A, n);
                     ShakerSort(A, n);
-                    cout << "С сортировкой: " << endl;
+                    cout << "\nС сортировкой: " << endl;
                     MinMaxAverageForSorted(A, n);
                     break;
                 case 3:
                     cout << "Без сортировки: " << endl;
                     MinMaxAverage(A, n);
                     combSort(A, n);
-                    cout << "С сортировкой: " << endl;
+                    cout << "\nС сортировкой: " << endl;
                     MinMaxAverageForSorted(A, n);
                     break;
                 case 4:
                     cout << "Без сортировки: " << endl;
                     MinMaxAverage(A, n);
                     insertSort(A, n);
-                    cout << "С сортировкой: " << endl;
+                    cout << "\nС сортировкой: " << endl;
                     MinMaxAverageForSorted(A, n);
                     break;
                 case 5:
@@ -289,11 +301,11 @@ int main() {
                     cout << "Без сортировки: " << endl;
                     MinMaxAverage(A, n);           
                     auto start1 = steady_clock::now();
-                    quicksort(A, end, begin);
+                    quicksort(A, endSort, beginSort);
                     auto end1 = steady_clock::now();
                     auto result1 = duration_cast<nanoseconds>(end1 - start1);
                     cout << "Сортировка заняла " << duration_cast<nanoseconds>(end1 - start1).count() << " нс" << endl;
-                    cout << "С сортировкой: " << endl;
+                    cout << "\nС сортировкой: " << endl;
                     MinMaxAverageForSorted(A, n);
                     break;
                 }
@@ -304,11 +316,11 @@ int main() {
             }while (choice2 != 6);
             if (choice2 == 6)
                 continue;
-        case 3:
+        case 4:
         {
             cout << "Введите число: ";
-            int num = checkInput(2147483647, -2147483648);
-            quicksort(A, end, begin);
+            int num = checkInput();
+            quicksort(A, endSort, beginSort);
             int count = 0;
             for (int i = 0; i < n; ++i) {
                 if (A[i] < num)
@@ -319,13 +331,13 @@ int main() {
             cout << "Чисел, меньше вашего: " << count << endl;
             break;
         }
-        case 4:
+        case 5:
         {
             cout << "Введите число: ";
-            int num = checkInput(2147483647, -2147483648);
-            quicksort(A, end, begin);
+            int num = checkInput();
+            quicksort(A, endSort, beginSort);
             int count = 0;
-            for (int i = n-1; i > 0; --i) {
+            for (int i = n-1; i >= 0; --i) {
                 if (A[i] > num)
                     count += 1;
                 else
@@ -334,11 +346,10 @@ int main() {
             cout << "Чисел, больше вашего: " << count << endl;
             break;
         }
-        case 5:
-        {
-            quicksort(A, end, begin);
+        case 6:
+        {            
             cout << "Введите число, которое вы ищите: ";
-            int value = checkInput(2147483647, -2147483648);
+            int value = checkInput();
             cout << "Поиск перебором: " << endl;
             int flag = 0;
             auto start = steady_clock::now();         
@@ -353,9 +364,11 @@ int main() {
             if (flag) {
                 cout << "Элемент был найден " << endl;
             }
-            else
+            else{
                 cout << "Элемент не найден" << endl;
-            cout << "Поиск занял " << duration_cast<nanoseconds>(end - start).count() << " нс" << endl;           
+            }
+            cout << "Поиск занял " << duration_cast<nanoseconds>(end - start).count() << " нс" << endl; 
+            quicksort(A, endSort, beginSort);
             cout << "Бинарный поиск: " << endl;
             auto startB = steady_clock::now();
             int reply = binarySearch(A, value, 0, size - 1);
@@ -370,14 +383,14 @@ int main() {
             cout << "Поиск занял " << duration_cast<nanoseconds>(endB - startB).count() << " нс" << endl;
             break;
         }
-        case 6:
+        case 7:
         {
             int index1, index2;
             cout << "Введите индексы элементов массива, которые вы хотите поменять местами" << endl;
             cout << "Первый индекс: ";
-            index1 = checkInput(99, -99);
+            index1 = checkInput();
             cout << "Второй индекс: ";
-            index2 = checkInput(99, -99);
+            index2 = checkInput();
             auto start1 = steady_clock::now();
             swap(A[index1], A[index2]);
             auto end1 = steady_clock::now();
@@ -390,20 +403,20 @@ int main() {
             break;
 
         }
-        case 7:
+        case 8:
         {
             cout << "Введите на какое значение уменьшить нечетные элементы массива: ";
-            int num = checkInput(2147483647, -2147483648);
+            int num = checkInput();
             int randNum = ((rand() % 9) + 1);
             cout << "Рандомное число: " << randNum << endl;
             for (int i = 1; i < n; i+=2) {
                 A[i] = (A[i] - num) * randNum;
             }
-            cout << "Измененный массив выглядт так: ";
+            cout << "Измененный массив выглядит так: ";
             for (int i = 0; i < n; ++i)
                 cout << A[i] << "; ";
             cout << endl;
-            cout << "Количество элеметов, которые нацело деляться на " << endl;
+            cout << "Количество элементов, которые нацело делятся на " << endl;
             int count = 0;
             cout << "1: " << n << endl;
             for (int i = 2; i < 9; ++i) {
@@ -417,8 +430,8 @@ int main() {
             break;
         }
         }
-        if (choice != 8)
+        if (choice != 9)
             system("pause");
-    }while (choice != 8);
+    }while (choice != 9);
     return 0;
 }
